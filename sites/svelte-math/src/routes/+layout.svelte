49baa5aebc8b5e6840b1d7f02f035003de0e6c96
Kelvin Soh <kelvinsjk@gmail.com>
2025-05-15 09:18:05 +0800

    moved svelte-math site from mathlified

---
+++ sites/svelte-math/src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import Nav from '$lib/Nav.svelte';
+  import Footer from '$lib/Footer.svelte';
+
+  import type { Snippet } from 'svelte';
+  let { children }: { children: Snippet } = $props();
+</script>
+
+<Nav />
+<main>
+  {@render children()}
+</main>
+<Footer />
+
+<style>
+  main {
+    padding: 1rem;
+    font-size: 1.25rem;
+    min-height: calc(100vh - 9.5rem);
+  }
+  :global(h1){
+    margin-top: 0rem;
+    font-size: 1.75em;
+  }
+</style>
